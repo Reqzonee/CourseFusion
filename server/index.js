aa53f65757a56693b0e7ac7ad43097304d01be48
Reqzonee <bhattparaj2@gmail.com>
2024-09-10 18:47:17 +0530

    added new virsion of nodemon__

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: JSON.parse(process.env.CORS_ORIGIN),
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
     maxAge: 14400,
   })
